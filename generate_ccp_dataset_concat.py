7b064e2627d4a2d813e85f6f1bc3fd80c4114140
SifanStephanie <sifanl@andrew.cmu.edu>
2020-05-03 14:20:36 -0400

    yueni use this generate cpp dataset to generate the concat mask

--- generate_ccp_dataset_concat.py
+++ generate_ccp_dataset_concat.py
@@ -21,12 +21,6 @@ def create_argument_parser():
 	# change cat 2_1 to cat 2_2
 	parser.add_argument('--cat21', type=str, default='coat', help='category 21')
 	parser.add_argument('--cat22', type=str, default='blouse', help='category 22')
-	# add more labeled data
-	parser.add_argument('--cat11_', type=str, default='pants', help='category 11_')
-	parser.add_argument('--cat12_', type=str, default='skirt', help='category 12_')
-
-	parser.add_argument('--cat21_', type=str, default='jacket', help='category 21_')
-	parser.add_argument('--cat22_', type=str, default='shirt', help='category 22_')
 	return parser
 
 def generate_ccp_dataset(args):
@@ -43,10 +37,10 @@ def generate_ccp_dataset(args):
 	args.save_root = Path(args.save_root)
 	args.save_root.mkdir()
 
-	generate_ccp_dataset_train(args, 'A', args.cat11, args.cat21, args.cat11_, args.cat21_)
-	generate_ccp_dataset_train(args, 'B', args.cat12, args.cat22, args.cat12_, args.cat22_)
-	generate_ccp_dataset_val(args, 'A', args.cat11, args.cat21, args.cat11_, args.cat21_)
-	generate_ccp_dataset_val(args, 'B', args.cat12, args.cat22, args.cat12_, args.cat22_)
+	generate_ccp_dataset_train(args, 'A', args.cat11, args.cat21)
+	generate_ccp_dataset_train(args, 'B', args.cat12, args.cat22)
+	generate_ccp_dataset_val(args, 'A', args.cat11, args.cat21)
+	generate_ccp_dataset_val(args, 'B', args.cat12, args.cat22)
 
 def generate_ccp_dataset_train(args, imset, cat1, cat2):
 	img_path = args.save_root / 'train{}'.format(imset)
@@ -65,13 +59,19 @@ def generate_ccp_dataset_train(args, imset, cat1, cat2):
 			if np.isin(ann, cat_id_2).sum() > 0:
 				img = Image.open(args.img_root / '{}.jpg'.format(ann_id))
 				img.save(img_path / '{}.png'.format(ann_id))
-				seg = (ann == cat_id_1 or ann == cat_id_2).astype('uint8')  # get segment of given category
-				seg = Image.fromarray(seg * 255)
-				seg.save(seg_path / '{}_0.png'.format(ann_id))
+				seg1 = (ann == cat_id_1).astype('uint8')  # get segment of given category
+				seg2 = (ann == cat_id_2).astype('uint8')
+				row,col = seg1.shape
+				for i in range(0,row):
+					for j in range(0,col):
+						if seg2[i][j]!=0:
+							seg1[i][j] = seg2[i][j]
+				seg1 = Image.fromarray(seg1 * 255)
+				seg1.save(seg_path / '{}_0.png'.format(ann_id))
 		pb.update(1)
 	pb.close()
 
-def generate_ccp_dataset_val(args, imset, cat1,cat2):
+def generate_ccp_dataset_val(args, imset, cat1, cat2):
 	img_path = args.save_root / 'val{}'.format(imset)
 	seg_path = args.save_root / 'val{}_seg'.format(imset)
 	img_path.mkdir()
@@ -80,12 +80,13 @@ def generate_ccp_dataset_val(args, imset, cat1,cat2):
 	cat_id_1 = get_cat_id(args.label_list, cat1)
 	cat_id_2 = get_cat_id(args.label_list, cat2)
 
+
 	pb = tqdm(total=len(args.img_ann_ids))
 	pb.set_description('val{}'.format(imset))
 	for ann_id in args.img_ann_ids:
 		ann = sio.loadmat(str(args.img_ann_root / '{}.mat'.format(ann_id)))['tags']
-		if np.isin(ann, cat_id_1).sum() > 0:
-			if np.isin(ann, cat_id_2).sum() > 0:
+		if np.isin(ann, cat_id_1).sum() > 0 or np.isin(ann, cat_id_1_).sum() > 0:
+			if np.isin(ann, cat_id_2).sum() > 0 or np.isin(ann, cat_id_2_).sum() > 0:
 				img = Image.open(args.img_root / '{}.jpg'.format(ann_id))
 				img.save(img_path / '{}.png'.format(ann_id))
 		pb.update(1)
